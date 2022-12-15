fb33abf4a850ed4e4a7da89d37637b0e1eff5b24
Matthew Arnold <marnold@redhat.com>
2022-12-15 09:45:47 -0500

    Set go-plugin handshake environment variable.
    Signed-off-by: Matthew Arnold <marnold@redhat.com>

--- plugindebug.go
+++ plugindebug.go
@@ -2,9 +2,17 @@ package plugindebug
 
 import (
 	"fmt"
+	"os"
 )
 
 func CreateDebugDummyScript(scriptPath string) error {
 	fmt.Printf("Saving gRPC socket path to %s", scriptPath)
+
+	if _, handshakePresent := os.LookupEnv("VELERO_PLUGIN"); !handshakePresent {
+		if err := os.Setenv("VELERO_PLUGIN", "hello"); err != nil {
+			return err
+		}
+	}
+
 	return nil
 }
